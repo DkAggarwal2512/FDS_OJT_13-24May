add21716a7a0a155c7105ef28620ddcc904e1bb9
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-05-24 15:17:19 +0530

    Add files via upload

---
+++ LoopsP.java
@@ -0,0 +1,42 @@
+// Do--while
+
+public class LoopsP{
+    public static void main(String[] args) {
+        int i = 12;
+        while(i<11){
+            System.out.println("IBM");
+        }
+        
+        do{
+            System.out.println("IBM");
+        }while(i<11);
+        }
+        
+        
+}
+
+// 2. For Loop
+
+public class For{
+    public static void main(String arg[]){
+        // (Initialisation; Condation; Updation)
+        // for (int a = 0; a <3; a = a + 1)
+        for(int a =0; a< 11; a++)
+        {
+            System.out.println(a+ " ");
+        }
+    }
+}
+
+
+// 3. While Loop
+
+public class While{
+    public static void main(String[] args) {
+        // (Condation)
+        int i = 0;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }}
+}
